9d8cc4a419e54e5915fe6101f92afb797cec7037
Akriti <vermakriti2905@gmail.com>
2023-03-09 22:31:25 +0000

    added methods and locators

--- src/main/java/com/qa/rockers/pages/OrderPage.java
+++ src/main/java/com/qa/rockers/pages/OrderPage.java
@@ -1,5 +1,18 @@
 package com.qa.rockers.pages;
 
+import org.openqa.selenium.By;
+
 public class OrderPage {
 
+	By price = By.className("price");
+	By orderCart = By.id("order123");
+	
+	public void getOrder() {
+		System.out.println("get order");
+	}
+	
+	public void getPrice() {
+		System.out.println("getPrice");
+	}
+	
 }
