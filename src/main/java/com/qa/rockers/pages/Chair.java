72a15a258e0a8f7f96f0f6a76d24508b60ccc934
Akriti <vermakriti2905@gmail.com>
2023-03-14 22:48:57 +0000

    fan added

---
+++ src/main/java/com/qa/rockers/pages/Chair.java
@@ -0,0 +1,11 @@
+package com.qa.rockers.pages;
+
+public class Chair {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Chair");
+
+	}
+
+}
---
+++ src/main/java/com/qa/rockers/pages/Fan.java
@@ -0,0 +1,11 @@
+package com.qa.rockers.pages;
+
+public class Fan {
+
+	public static void main(String[] args) {
+		System.out.println("fan");
+		int a=10;
+		System.out.println(""+a);
+	}
+
+}
